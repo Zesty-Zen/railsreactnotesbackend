68763c60a78d8ad6d71787ae2d7e06cbdd98cf29
zesty-zen <ibrahimmamoon7@gmail.com>
2024-05-17 12:01:20 +0500

    render-build.sh renewed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+# Install gems
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
+# You can remove the assets-related commands
